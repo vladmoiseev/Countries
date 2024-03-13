caa37e496b446c443d5ef671af8dd50cdecfaa32
vladulnik <vladislavmoiseev9@gmail.com>
2024-03-13 14:43:45 +0300

    Update CityAlreadyExistException.java

--- src/main/java/com/example/countries/exception/CityAlreadyExistException.java
+++ src/main/java/com/example/countries/exception/CityAlreadyExistException.java
@@ -1,7 +1,5 @@
 package com.example.countries.exception;
 
-import com.example.countries.entity.City;
-
 public class CityAlreadyExistException extends Exception{
     public CityAlreadyExistException(String message) {
         super(message);
